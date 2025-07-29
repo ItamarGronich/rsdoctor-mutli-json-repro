b8db7073c0862b94f3caef8ea31f17f043317e8e
ItamarGronich <itamar.gronich@fiverr.com>
2025-07-30 00:59:32 +0300

    Add name to config.

--- rspack.config.ts
+++ rspack.config.ts
@@ -9,6 +9,7 @@ type Options = {
 
 function createConfig(options: Options) {
   return defineConfig({
+    name: options.target,
     target: options.target,
     mode: process.env.NODE_ENV === "production" ? "production" : "development",
     devtool: "source-map",
